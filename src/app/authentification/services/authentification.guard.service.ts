90d596193ab22eb23c758520c6781a61cf5df209
Mouslih0 <maromouslih@gmail.com>
2024-02-16 16:01:13 +0100

    last commit 2

--- src/app/authentification/services/authentification.guard.service.ts
+++ src/app/authentification/services/authentification.guard.service.ts
@@ -27,7 +27,19 @@ export class AuthentificationGuardService implements CanActivate {
         {
           return this.router.createUrlTree(['admin/dashboard'])
         }
-        //return true;
+        // if(isAuth)
+        // {
+        //   if(user.roles.includes(route.data['role'])) return true;
+        //   else if (user.roles.includes('Admin'))
+        //   {
+        //     return this.router.createUrlTree(['admin/dashboard']);
+        //   }else if(user.roles.includes('Technicien'))
+        //   {
+        //     return this.router.createUrlTree(['admin/patients']);
+        //   }else if(user.roles.includes('Responsable'))
+        //   {
+        //     return this.router.createUrlTree(['admin/dashboard'])
+        //   }
       }
 
       return this.router.createUrlTree(['auth']);
